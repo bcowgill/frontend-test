2f7d67233bc2ddc5f2caac9c773f5347848a029f
Brent S.A. Cowgill <968686+bcowgill@users.noreply.github.com>
2021-03-03 10:38:45 +0000

    wip Fixed react jsx-filename-extension error
    For components renamed the file, for test plans added an override.

--- src/Autocomplete.test.js
+++ src/Autocomplete.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
